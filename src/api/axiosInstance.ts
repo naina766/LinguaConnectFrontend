93f0cc564a75e9d152ae54aaeb62b9c15015eced
naina766 <nainavarshney842@gmail.com>
2025-11-17 22:51:31 +0530

    Change API base URL to production endpoint

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4001", // your backend URL
+  baseURL: "https://linguaconnect-hackathon.onrender.com", // your backend URL
   withCredentials: true,
 });
 
